e43b3658c8e2b1e1b432836de7446ae0a9e54317
Daniela Acero <danielalizethacero@gmail.com>
2025-02-05 18:37:32 -0500

    Cambios estéticos

--- Ejercicios1.py
+++ Ejercicios1.py
@@ -1,4 +1,4 @@
-# 1. Recibir el nombre del usuario por teclado y mostrar por pantalla "Hola (nombre)"
+""" 1. Recibir el nombre del usuario por teclado y mostrar por pantalla "Hola (nombre)""""
 
 nombre = str(input("Escribe tu nombre "))
 print("Hola", nombre)
@@ -17,12 +17,12 @@ print(numero*8)
 print(numero*9)
 print(numero*10)
 
-# 3. Calcular el resultado de la siguiente operación y mostrarlo en pantalla
+""" 3. Calcular el resultado de la siguiente operación y mostrarlo en pantalla"""
 
 resultado= ((45-3**2)**(1/2))/(2*3) - 1
 print(resultado)
 
-# 4. Recibir un número de tres cifras y mostrar sus dígitos del primero al último
+""" 4. Recibir un número de tres cifras y mostrar sus dígitos del primero al último"""
 
 cifras = int(input("Ingrese un número de 3 cifras: "))
 digito1 = (cifras%1000)//100
@@ -30,8 +30,8 @@ digito2 = (cifras%100)//10
 digito3 = cifras%10
 print( digito1, digito2, digito3)
 
-# 5. En un videojuego el personaje principal recibe un aumento de daño del 10% con cada nivel que sube.
-# Recibir por teclado el nivel y daño base del personaje y mostrar cuál será su daño final.
+""" 5. En un videojuego el personaje principal recibe un aumento de daño del 10% con cada nivel que sube.
+    Recibir por teclado el nivel y daño base del personaje y mostrar cuál será su daño final."""
 
 nivel = int(input("Ingrese su nivel: "))
 dahnoBase = float(input("Ingrese el daño base: "))
--- Ejercicios2.py
+++ Ejercicios2.py
@@ -1,68 +1,68 @@
-# 1. Para comprar una poción roja, Link necesita 30 rupias. Hacer un programa que permita ingresar un número
-# de rupias y mostrar su puede comprar o no la poción, en caso de que no, decir cuantas rupias le faltan.
+""" 1. Para comprar una poción roja, Link necesita 30 rupias. Hacer un programa que permita ingresar un número
+    de rupias y mostrar su puede comprar o no la poción, en caso de que no, decir cuantas rupias le faltan."""
 
-# rupias = int(input("Ingrese el número de rupias: "))
-# faltante = 0
+rupias = int(input("Ingrese el número de rupias: "))
+faltante = 0
 
-# if rupias<30:
-#     faltante = 30- rupias
-#     print("Faltan",faltante,"rupias para comprar la poción")
-#     print(f"No puedes comprar la poción, te faltan {30-rupias} rupias)  #Otra forma de escribirlo
-# else:
-#     print("Puede comprar la poción")
+if rupias<30:
+    faltante = 30- rupias
+    print("Faltan",faltante,"rupias para comprar la poción")
+    print(f"No puedes comprar la poción, te faltan {30-rupias} rupias)  #Otra forma de escribirlo
+else:
+    print("Puede comprar la poción")
 
 
-# 2. Hacer un programa que permita ingresar el nombre de un estudiante y 3 notas con valor de 0 a 5.
-# Calcular el promedio y si es mayor o igual a 3 mostrar por pantalla "Aprobado", de lo contrario "Reprobado".
+""" 2. Hacer un programa que permita ingresar el nombre de un estudiante y 3 notas con valor de 0 a 5.
+    Calcular el promedio y si es mayor o igual a 3 mostrar por pantalla "Aprobado", de lo contrario "Reprobado"."""
 
-# nombre = input("Ingrese el nombre: ")
-# nota1 = float(input("Ingrese la primera nota: "))
-# nota2 = float(input("Ingrese la segunda nota: "))
-# nota3 = float(input("Ingrese la tercera nota: "))
-# promedio = (nota1+nota2+nota3)/3
+nombre = input("Ingrese el nombre: ")
+nota1 = float(input("Ingrese la primera nota: "))
+nota2 = float(input("Ingrese la segunda nota: "))
+nota3 = float(input("Ingrese la tercera nota: "))
+promedio = (nota1+nota2+nota3)/3
 
-# if promedio<3:
-#     print("Reprobado con",round(promedio,2))  #round para limitar el número de decimales del resultado
-# else:
-#     print("Aprobado con",round(promedio,2))
+if promedio<3:
+    print("Reprobado con",round(promedio,2))  #round para limitar el número de decimales del resultado
+else:
+    print("Aprobado con",round(promedio,2))
 
-# 3. Realizar un problema que permita ingresar una contraseña de por lo menos 7 caracteres. Luego preguntarla
-# y verificar si es correcta o no, en caso de no serlo, seguir preguntando hasta que se ingrese la correcta.
+""" 3. Realizar un problema que permita ingresar una contraseña de por lo menos 7 caracteres. Luego preguntarla
+    y verificar si es correcta o no, en caso de no serlo, seguir preguntando hasta que se ingrese la correcta."""
 
-# password= input("Cree una contraseña de almenos 7 caracteres: ")
-# contador = 0
-# caracteres = False
+password= input("Cree una contraseña de almenos 7 caracteres: ")
+contador = 0
+caracteres = False
 
-# while caracteres==False:
-#     for x in password:
-#         contador +=1
-#     if contador>=7:
-#         print("Cantidad caracteres válida")
-#         caracteres = True
-#     else:
-#         print("La contraseña debe tener al menos 7 caracteres")
-#         password = input("Cree una contraseña de almenos 7 caracteres: ")
-#         caracteres = False
+while caracteres==False:
+    for x in password:
+        contador +=1
+    if contador>=7:
+        print("Cantidad caracteres válida")
+        caracteres = True
+    else:
+        print("La contraseña debe tener al menos 7 caracteres")
+        password = input("Cree una contraseña de almenos 7 caracteres: ")
+        caracteres = False
 
-# verificar = input("Ingrese la contraseña: ")
+verificar = input("Ingrese la contraseña: ")
 
-# while password!=verificar:
-#     print("Contraseña incorrecta")
-#     verificar = input("Ingrese la contraseña: ")
+while password!=verificar:
+    print("Contraseña incorrecta")
+    verificar = input("Ingrese la contraseña: ")
 
-# print("Contraseña correcta")
+print("Contraseña correcta")
 
-# 4. Mostrar por pantalla los primeros n términos de la serie Fibonacci que comienza en 0 y 1 y genera los
-# siguientes términos con la suma de los dos anteriores, en donde n es un número ingresado por el usuario.
+""" 4. Mostrar por pantalla los primeros n términos de la serie Fibonacci que comienza en 0 y 1 y genera los
+    siguientes términos con la suma de los dos anteriores, en donde n es un número ingresado por el usuario."""
 
-# n = int(input("Ingrese un número: "))
-# n1 = 0
-# n2 = 1
-# for x in range(n):
-#     n3 = n1+n2
-#     print(n3)
-#     n1=n2
-#     n2=n3
+n = int(input("Ingrese un número: "))
+n1 = 0
+n2 = 1
+for x in range(n):
+    n3 = n1+n2
+    print(n3)
+    n1=n2
+    n2=n3
 
 """ 5. Crear un menú que permita:
     a. Crear un personaje, preguntando nombre, tipo y atributos (ataque, velocidad, magia)
